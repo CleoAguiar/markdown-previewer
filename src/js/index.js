13017a95b96e249a1b28a2d62c3def858d46ef28
Cleo Aguiar <cleotaisas@gmail.com>
2019-04-04 07:14:33 -0300

    refactor: change placeholder text

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,36 @@ const placeholder =
 `# Welcome to my React Markdown Previewer!
 
 ## This is my Second Front End Project FreeCodeCamp
+
+Heading
+=======
+
+Sub-heading
+-----------
+
+### Another deeper heading
+
+Paragraphs are separated
+by a blank line.
+
+Leave 2 spaces at the end of a line to do a  
+line break
+
+Text attributes *italic*, **bold**,~~strikethrough~~ .
+
+Shopping list:
+
+* apples
+* oranges
+* pears
+Numbered list:
+
+1. apples
+2. oranges
+3. pears
+
+The rain---not the reign---in
+Spain.
 `
 
 
